30f7dff0e4bc5bfc287f7101bd443eab47efdcc0
paventhan89 <venthan4805@gmail.com>
2019-11-28 21:34:04 +0530

    Working: API: Login, Register. Token generation successful, Register successful, Login successful

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,23 @@
+import * as crypto from "crypto";
 import * as express from "express";
+import jwt from "jsonwebtoken";
 import pgPromise from "pg-promise";
 
 export const register = (app: express.Application) => {
-    // const oidc = app.locals.oidc;
+
+    const PASSWORD_LENGTH = 256;
+    const SALT_LENGTH = 64;
+    const ITERATIONS = 10000;
+    const DIGEST = "sha256";
+    const BYTE_TO_STRING_ENCODING = "hex"; // this could be base64, for instance
+
+    // tslint:disable-next-line:interface-name
+    interface PersistedPassword {
+        salt: string;
+        hash: string;
+        iterations: number;
+    }
+
     const port = parseInt(process.env.PGPORT || "5432", 10);
     const config = {
         database: process.env.PGDATABASE || "postgres",
@@ -14,13 +29,98 @@ export const register = (app: express.Application) => {
     const pgp = pgPromise();
     const db = pgp(config);
 
-    app.post(`/api/login`, async (req: any, res) => {
-        // Check if username and password are set
+    function generateHashPassword(password: string): Promise<PersistedPassword> {
+        return new Promise<PersistedPassword>((accept, reject) => {
+            const salt = crypto.randomBytes(SALT_LENGTH).toString(BYTE_TO_STRING_ENCODING);
+            crypto.pbkdf2(password, salt, ITERATIONS, PASSWORD_LENGTH, DIGEST, (error, hash) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    accept({
+                        salt,
+                        hash: hash.toString(BYTE_TO_STRING_ENCODING),
+                        iterations: ITERATIONS,
+                    });
+                }
+            });
+        });
+    }
 
-        // Test
-        const {username, password} = req.body;
-        if (!(username && password)) {
-            res.status(400).send();
+    function verifyPassword(persistedPassword: PersistedPassword, passwordAttempt: string): Promise<boolean> {
+        return new Promise<boolean>((accept, reject) => {
+            // tslint:disable-next-line:max-line-length
+            console.log(passwordAttempt + " " + persistedPassword.salt + " " + persistedPassword.iterations + " " + PASSWORD_LENGTH + " " + DIGEST);
+            // tslint:disable-next-line:max-line-length radix
+            crypto.pbkdf2(passwordAttempt, persistedPassword.salt, persistedPassword.iterations, PASSWORD_LENGTH, DIGEST, (error, hash) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    console.log(+persistedPassword.hash + " " + hash.toString(BYTE_TO_STRING_ENCODING));
+                    accept(persistedPassword.hash === hash.toString(BYTE_TO_STRING_ENCODING));
+                }
+            });
+        });
+    }
+
+    app.post(`/api/users/register`, async (req: any, res) => {
+        // tslint:disable-next-line: no-console
+        console.log(req.body);
+
+        try {
+            const emailExpression = /\S+@\S+\.\S+/;
+            if (!req.body.email || !req.body.password) {
+                return res.status(400).send({message: "Some values are missing"});
+            }
+            if (!emailExpression.test(String(req.body.email).toLowerCase())) {
+                return res.status(400).send({message: "Please enter a valid email address"});
+            }
+
+            // @ts-ignore
+            const hashedPassword = await generateHashPassword<PersistedPassword>(req.body.password);
+
+            const result = await db.one(`INSERT INTO users( username, email, password, salt, iterations )
+            VALUES( $[username], $[email], $[password], $[salt], $[iterations] )
+            RETURNING id`, {
+                email: req.body.email,
+                password: hashedPassword.hash,
+                username: req.body.username,
+                salt: hashedPassword.salt,
+                iterations: hashedPassword.iterations,
+            });
+            return res.json({result});
+        } catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err);
+            res.json({error: err.message || err});
+        }
+    });
+
+    app.post(`/api/users/login`, async (req: any, res) => {
+        try {
+            const emailExpression = /\S+@\S+\.\S+/;
+            if (!req.body.email || !req.body.password) {
+                return res.status(400).send({message: "Some values are missing"});
+            }
+            if (!emailExpression.test(String(req.body.email).toLowerCase())) {
+                return res.status(400).send({message: "Please enter a valid email address"});
+            }
+            const query = "SELECT password as hash, salt, iterations FROM users WHERE email = $1";
+
+            const rows = await db.one(query, [req.body.email]);
+
+            if (!rows) {
+                return res.status(400).send({message: "The credentials you provided is incorrect..."});
+            }
+
+            const result = await verifyPassword(rows, req.body.password);
+
+            if (!result) {
+                return res.status(400).send({message: "The credentials you provided is incorrect"});
+            }
+            const token = jwt.sign({id: rows.id}, process.env.JWTSECRET, {expiresIn: 60 * 60});
+            return res.status(200).send({token});
+        } catch (err) {
+            return res.status(400).send(err);
         }
 
     });
