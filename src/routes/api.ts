6df422633ef08c84759cdf9df9bc6515f82658c0
paventhan89 <venthan4805@gmail.com>
2019-11-28 05:51:47 +0000

    Working: api route is added

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,9 @@
 import * as express from "express";
 import pgPromise from "pg-promise";
 
-export const register = ( app: express.Application ) => {
+export const register = (app: express.Application) => {
     // const oidc = app.locals.oidc;
-    const port = parseInt( process.env.PGPORT || "5432", 10 );
+    const port = parseInt(process.env.PGPORT || "5432", 10);
     const config = {
         database: process.env.PGDATABASE || "postgres",
         host: process.env.PGHOST || "localhost",
@@ -12,12 +12,21 @@ export const register = ( app: express.Application ) => {
     };
 
     const pgp = pgPromise();
-    const db = pgp( config );
+    const db = pgp(config);
 
-    app.get( `/api/guitars/all`, async ( req: any, res ) => {
+    app.post(`/api/login`, async (req: any, res) => {
+        // Check if username and password are set
+        const { username, password } = req.body;
+        if (!(username && password)) {
+            res.status(400).send();
+        }
+
+    });
+
+    app.get(`/api/guitars/all`, async (req: any, res) => {
         try {
             const userId = req.userContext.userinfo.sub;
-            const guitars = await db.any( `
+            const guitars = await db.any(`
                 SELECT
                     id
                     , brand
@@ -26,38 +35,38 @@ export const register = ( app: express.Application ) => {
                     , color
                 FROM    guitars
                 WHERE   user_id = $[userId]
-                ORDER BY year, brand, model`, { userId } );
-            return res.json( guitars );
-        } catch ( err ) {
+                ORDER BY year, brand, model`, { userId });
+            return res.json(guitars);
+        } catch (err) {
             // tslint:disable-next-line:no-console
             console.error(err);
-            res.json( { error: err.message || err } );
+            res.json({ error: err.message || err });
         }
-    } );
+    });
 
-    app.get( `/api/guitars/total`, async ( req: any, res ) => {
+    app.get(`/api/guitars/total`, async (req: any, res) => {
         try {
             const userId = req.userContext.userinfo.sub;
-            const total = await db.one( `
+            const total = await db.one(`
             SELECT  count(*) AS total
             FROM    guitars
-            WHERE   user_id = $[userId]`, { userId }, ( data: { total: number } ) => {
+            WHERE   user_id = $[userId]`, { userId }, (data: { total: number }) => {
                 return {
                     total: +data.total
                 };
-            } );
-            return res.json( total );
-        } catch ( err ) {
+            });
+            return res.json(total);
+        } catch (err) {
             // tslint:disable-next-line:no-console
             console.error(err);
-            res.json( { error: err.message || err } );
+            res.json({ error: err.message || err });
         }
-    } );
+    });
 
-    app.get( `/api/guitars/find/:search`, async ( req: any, res ) => {
+    app.get(`/api/guitars/find/:search`, async (req: any, res) => {
         try {
             const userId = req.userContext.userinfo.sub;
-            const guitars = await db.any( `
+            const guitars = await db.any(`
                 SELECT
                     id
                     , brand
@@ -67,35 +76,35 @@ export const register = ( app: express.Application ) => {
                 FROM    guitars
                 WHERE   user_id = $[userId]
                 AND   ( brand ILIKE $[search] OR model ILIKE $[search] )`,
-                { userId, search: `%${ req.params.search }%` } );
-            return res.json( guitars );
-        } catch ( err ) {
+                { userId, search: `%${req.params.search}%` });
+            return res.json(guitars);
+        } catch (err) {
             // tslint:disable-next-line:no-console
             console.error(err);
-            res.json( { error: err.message || err } );
+            res.json({ error: err.message || err });
         }
-    } );
+    });
 
-    app.post( `/api/guitars/add`, async ( req: any, res ) => {
+    app.post(`/api/guitars/add`, async (req: any, res) => {
         try {
             const userId = req.userContext.userinfo.sub;
-            const id = await db.one( `
+            const id = await db.one(`
                 INSERT INTO guitars( user_id, brand, model, year, color )
                 VALUES( $[userId], $[brand], $[model], $[year], $[color] )
                 RETURNING id;`,
-                { userId, ...req.body  } );
-            return res.json( { id } );
-        } catch ( err ) {
+                { userId, ...req.body });
+            return res.json({ id });
+        } catch (err) {
             // tslint:disable-next-line:no-console
             console.error(err);
-            res.json( { error: err.message || err } );
+            res.json({ error: err.message || err });
         }
-    } );
+    });
 
-    app.post( `/api/guitars/update`, async ( req: any, res ) => {
+    app.post(`/api/guitars/update`, async (req: any, res) => {
         try {
             const userId = req.userContext.userinfo.sub;
-            const id = await db.one( `
+            const id = await db.one(`
                 UPDATE guitars
                 SET brand = $[brand]
                     , model = $[model]
@@ -106,29 +115,29 @@ export const register = ( app: express.Application ) => {
                     AND user_id = $[userId]
                 RETURNING
                     id;`,
-                { userId, ...req.body  } );
-            return res.json( { id } );
-        } catch ( err ) {
+                { userId, ...req.body });
+            return res.json({ id });
+        } catch (err) {
             // tslint:disable-next-line:no-console
             console.error(err);
-            res.json( { error: err.message || err } );
+            res.json({ error: err.message || err });
         }
-    } );
+    });
 
-    app.delete( `/api/guitars/remove/:id`, async ( req: any, res ) => {
-        try {
-            const userId = req.userContext.userinfo.sub;
-            const id = await db.result( `
-                DELETE
-                FROM    guitars
-                WHERE   user_id = $[userId]
-                AND     id = $[id]`,
-                { userId, id: req.params.id  }, ( r ) => r.rowCount );
-            return res.json( { id } );
-        } catch ( err ) {
-            // tslint:disable-next-line:no-console
-            console.error(err);
-            res.json( { error: err.message || err } );
-        }
-    } );
+    // app.delete(`/api/guitars/remove/:id`, async (req: any, res) => {
+    //     try {
+    //         const userId = req.userContext.userinfo.sub;
+    //         const id = await db.result(`
+    //             DELETE
+    //             FROM    guitars
+    //             WHERE   user_id = $[userId]
+    //             AND     id = $[id]`,
+    //             { userId, id: req.params.id }, (r) => r.rowCount);
+    //         return res.json({ id });
+    //     } catch (err) {
+    //         // tslint:disable-next-line:no-console
+    //         console.error(err);
+    //         res.json({ error: err.message || err });
+    //     }
+    // });
 };
